43e236085c7c6b629e9c90e08ed1a9579b9fc6ff
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-01-06 19:15:32 +0100

    Jobber med layout

---
+++ components/Layout.js
@@ -0,0 +1,18 @@
+
+const Layout = ({Children}) => {
+
+
+    return(
+        <>
+        <h1> Bass4Nation Testpage</h1>
+        <main>
+            {/* <Children /> */}
+        </main>
+        <footer>
+            <p>Her er footer</p>
+        </footer>
+        </>
+    )
+}
+
+export default Layout
