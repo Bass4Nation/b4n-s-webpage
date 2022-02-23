23de65722a3a8b9a2b42a42d1a415795fe69264a
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-02-23 17:01:53 +0100

    Trying to link github

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+const Layout = ({ Children }) => {
+  return (
+    <>
+      <h1> Bass4Nation Testpage</h1>
+      <main>{/* <Children /> */}</main>
+      <footer>
+        <section>
+          <section>
+            <p>Git this page</p>
+            <Link href="https://github.com/Bass4Nation/b4n-s-webpage">
+              <>
+                <Image src="/gitlogo.png" width="50" height="50" alt="Github" />
+              </>
+            </Link>
+          </section>
+          <section>
+            <p>Git most of my stuff</p>
+            <Link href="https://github.com/Bass4Nation">
+              <>
+                <Image src="/gitlogo.png" width="50" height="50" alt="Github" />
+              </>
+            </Link>
+          </section>
+        </section>
+      </footer>
+    </>
+  );
+};
 
-const Layout = ({Children}) => {
-
-
-    return(
-        <>
-        <h1> Bass4Nation Testpage</h1>
-        <main>
-            {/* <Children /> */}
-        </main>
-        <footer>
-            <p>Her er footer</p>
-        </footer>
-        </>
-    )
-}
-
-export default Layout
+export default Layout;
