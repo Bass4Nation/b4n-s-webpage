0cd7d4f45170e66ead89a4f2d339910e17305f2a
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-02-24 10:04:52 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -9,18 +9,14 @@ const Layout = ({ Children }) => {
         <section>
           <section>
             <p>Git this page</p>
-            <Link href="https://github.com/Bass4Nation/b4n-s-webpage">
-              <>
-                <Image src="/gitlogo.png" width="50" height="50" alt="Github" />
-              </>
+            <Link href="https://github.com/Bass4Nation/b4n-s-webpage" passHref>
+                <Image src="/gitlogo.png" width="50" height="50" alt="Github this page" />
             </Link>
           </section>
           <section>
             <p>Git most of my stuff</p>
-            <Link href="https://github.com/Bass4Nation">
-              <>
-                <Image src="/gitlogo.png" width="50" height="50" alt="Github" />
-              </>
+            <Link href="https://github.com/Bass4Nation"  passHref>
+                <Image src="/gitlogo.png" width="50" height="50" alt="Github User" />
             </Link>
           </section>
         </section>
