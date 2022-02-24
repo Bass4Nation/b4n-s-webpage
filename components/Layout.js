6c36e747a454eb51801348ac97cc2a52a10d7478
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-02-24 10:54:47 +0100

    Added possibility for children element in Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,36 @@
 import Link from "next/link";
 import Image from "next/image";
-const Layout = ({ Children }) => {
+import styles from "../styles/Layout.module.css";
+
+const Layout = ( {children} ) => {
   return (
     <>
       <h1> Bass4Nation Testpage</h1>
-      <main>{/* <Children /> */}</main>
+      <main>
+       {children}
+       </main>
       <footer>
         <section>
-          <section>
+          <section className={styles.footer_element}>
             <p>Git this page</p>
             <Link href="https://github.com/Bass4Nation/b4n-s-webpage" passHref>
-                <Image src="/gitlogo.png" width="50" height="50" alt="Github this page" />
+              <Image
+                src="/gitlogo.png"
+                width="50"
+                height="50"
+                alt="Github this page"
+              />
             </Link>
           </section>
-          <section>
+          <section className={styles.footer_element}>
             <p>Git most of my stuff</p>
-            <Link href="https://github.com/Bass4Nation"  passHref>
-                <Image src="/gitlogo.png" width="50" height="50" alt="Github User" />
+            <Link href="https://github.com/Bass4Nation" passHref>
+              <Image
+                src="/gitlogo.png"
+                width="50"
+                height="50"
+                alt="Github User"
+              />
             </Link>
           </section>
         </section>
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
+import "../styles/globals.css";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return(
+  return (
     <>
-  <Layout>
-  <Component {...pageProps} />
-  </Layout>
-  </>
-  )
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
