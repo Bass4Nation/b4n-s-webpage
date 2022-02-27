ba9fffb9d7cb130f28066403080a94030101ae79
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-02-27 15:26:36 +0100

    Update _app.js
    added under construction popup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
+
+  const [ constState, setConstState] = useState(true)
+
+  const [ langState, setLangState] = useState("en")
+
+  if(constState == true){
+    alert("This page is under construction.")
+  }
+
+
   return (
     <>
       <Layout>
