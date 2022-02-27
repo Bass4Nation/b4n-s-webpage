8885d8cd8a33f84a2229b238048e58d23ac26c83
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-02-27 15:51:19 +0100

    Update aboutme.js
    More content

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -1,10 +1,16 @@
 const AboutMe = () => {
+  return (
+    <>
+      <h3>About me page</h3>
+      <p>
+        Hi. This is Bass4Nation AKA Kristoffer S Søderkvist&apos;s portfolio
+        page. On top of this page is is a couple of choice for what you want to
+        read about. And in the bottom of this page is two Github links. First
+        one is for this project, and the second one is for all my public github
+        projects.
+      </p>
+    </>
+  );
+};
 
-    return (
-        <>
-            <h3>About me page</h3>
-        </>
-    )
-}
-
-export default AboutMe
+export default AboutMe;
