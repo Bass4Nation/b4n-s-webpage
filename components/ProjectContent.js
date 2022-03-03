6f647b5f6b1cf27feee6e0d5175a23bcfb0be863
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-03-03 12:34:35 +0100

    added some content projects

--- components/ProjectContent.js
+++ components/ProjectContent.js
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import styles from "../styles/Main.module.css";
 
 
-const ProjectContent = ( {title, description, projectLink} ) => {
+
+const ProjectContent = ( {title, codeLang, description, projectLink} ) => {
 
 
     return (
         <>
         <section>
             <p>Title: {title}</p>
+            <p>Language used: {codeLang}</p>
             <p>Description: {description}</p>
-            <p>Link to this project: <Link href={projectLink}>{projectLink}</Link></p>
+            <p  >Link to this project: <Link href={projectLink}><a className={styles.projectLinks}>{projectLink}</a></Link></p>
         </section>
         
         </>
--- pages/projects.js
+++ pages/projects.js
@@ -6,15 +6,23 @@ const Projects = () =>{
 
     const projectData = [{
         "name": "B4N.no Webpage",
+        "codeLang": "Next.js, HTML, CSS5, JavaScript",
         "description": "This webpage. Wanted to make this webpage because it could show some of my coding skills.",
         "link": "https://github.com/Bass4Nation/b4n-s-webpage"
-    }]
+    },
+     {
+        "name": "Tello SDK Drone Controller. ",
+        "codeLang": "Next.js, HTML, CSS5, JavaScript",
+        "description": "Got a small programmable drone at home. It uses a UDP client to send information on what it should do." +
+        " This project",
+        "link": "Not uploaded to Github yet"
+        },]
 
     return (
         <>
         <h3> Projects page</h3>
         {projectData.map((data) => 
-            <ProjectContent title={data.name} description={data.description} projectLink={data.link}/>
+            <ProjectContent title={data.name} codeLang={data.codeLang} description={data.description} projectLink={data.link}/>
         )}
         </>
     )
