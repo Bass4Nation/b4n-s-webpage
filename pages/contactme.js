79fa12c5c88c5eb5ca6c20277d5b0914d47888fb
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-03-15 17:22:06 +0100

    Update contactme.js

--- pages/contactme.js
+++ pages/contactme.js
@@ -5,9 +5,8 @@ const ContactMe = () => {
         <>
         <h3>Contact infomation page</h3>
         <p>Name: Kristoffer Snopestad Søderkvist</p>
-        <p>Phonenumber: Will not publish it yet</p>
+        <p>Phonenumber: 0047 98037897</p>
         <p>Preferred e-mail: 240_lfc@hotmail.no</p>
-        <p>But have a less used e-mail: bass4nation@gmail.com</p>
         </>
     )
 }
