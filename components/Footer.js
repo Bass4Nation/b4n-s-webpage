beefdb3a5411a76f570b931342a8aa583eb6b169
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-03-15 10:22:02 +0100

    Changed how links works

--- components/Footer.js
+++ components/Footer.js
@@ -6,28 +6,29 @@ const Footer = () => {
   return (
     <>
       <section className={styles.flex_footer}>
-        <section className={styles.footer_element}>
-          <p>Git this page</p>
-          <Link href="https://github.com/Bass4Nation/b4n-s-webpage" passHref>
+        <Link href="https://github.com/Bass4Nation/b4n-s-webpage" passHref>
+          <section className={styles.footer_element}>
+            <p>Git this page</p>
             <Image
               src="/gitlogo.png"
               width="50"
               height="50"
               alt="Github this page"
             />
-          </Link>
-        </section>
-        <section className={styles.footer_element}>
-          <p>Git most of my stuff</p>
-          <Link href="https://github.com/Bass4Nation" passHref>
+          </section>
+        </Link>
+
+        <Link href="https://github.com/Bass4Nation" passHref>
+          <section className={styles.footer_element}>
+            <p>Git most of my stuff</p>
             <Image
               src="/gitlogo.png"
               width="50"
               height="50"
               alt="Github User"
             />
-          </Link>
-        </section>
+          </section>
+        </Link>
       </section>
     </>
   );
--- components/Nav.js
+++ components/Nav.js
@@ -3,64 +3,57 @@ import styles from "../styles/Nav.module.css";
 import { useState } from "react";
 
 const Nav = () => {
-
-
   const [screenState, setScreenState] = useState(true);
 
-const fullscreen = (
-  <>
+  const fullscreen = (
+    <>
+      <Link href="/" passHref>
         <section className={styles.nav_elements}>
-          <Link href="/" passHref>
-            Home
-          </Link>
+          <p>Home</p>
         </section>
-        <section className={styles.nav_elements}>
-          <Link href="/projects" passHref>
-            Projects
-          </Link>
-        </section>
-
-        <section className={styles.nav_elements}>
-          <Link href="/aboutme" passHref>
-            About me
-          </Link>
-        </section>
-
-        <section className={styles.nav_elements}>
-          <Link href="/contactme" passHref>
-            Contact me
-          </Link>
-        </section>
-
-    <section className={styles.nav_elements}>
-      <Link href="/firebase" passHref>
-        Firebase
       </Link>
-    </section>
-    <section className={styles.nav_elements}>
-      <Link href="/firebase" passHref>
-        MySQL
+
+      <Link href="/projects" passHref>
+        <section className={styles.nav_elements}>
+          <p>Projects</p>
+        </section>
       </Link>
-    </section>
 
+      <Link href="/aboutme" passHref>
+        <section className={styles.nav_elements}>
+          <p>About me</p>
+        </section>
+      </Link>
 
-  </>
-);
-const mobileMenu = (
-  <>
-  <a>Menu</a>
-  </>
-);
+      <Link href="/contactme" passHref>
+        <section className={styles.nav_elements}>
+          <p>Contact me</p>
+        </section>
+      </Link>
 
-const mobileMenuPart1 = (
-  <>
-  
-  </>
-)
+      <Link href="/firebase" passHref>
+        <section className={styles.nav_elements}>
+          <p>Firebase</p>
+        </section>
+      </Link>
+
+      <Link href="/firebase" passHref>
+        <section className={styles.nav_elements}>
+          <p>MySQL</p>
+        </section>
+      </Link>
+    </>
+  );
+  const mobileMenu = (
+    <>
+      <a>Menu</a>
+    </>
+  );
+
+  const mobileMenuPart1 = <></>;
 
   return (
     <>
-
       <section className={styles.nav_background}>
         <a className={styles.main_title}>B4N&apos;s Portfolio Page</a>
         <section className={styles.nav_section}>
