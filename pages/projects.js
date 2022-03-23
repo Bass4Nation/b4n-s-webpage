c4bc2c9beb8aa73c71ecc57789bb566ccf2fcc29
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-03-23 10:35:41 +0100

    Update projects.js
    Added*
    * All repositories.
    *Link Tello Drone

--- pages/projects.js
+++ pages/projects.js
@@ -17,7 +17,7 @@ const Projects = () => {
       description:
         "Got a small programmable drone at home. It uses a UDP client to send information on what it should do." +
         " etl",
-      link: "https://github.com/Bass4Nation",
+      link: "https://github.com/Bass4Nation/Tello-Drone",
     },
     {
       key: "3",
@@ -27,7 +27,17 @@ const Projects = () => {
         "Self driven RC car, 2D lidar mapping, also possible with manuell control",
       link: "https://github.com/fredlau95/AutonomousRacecar",
     },
+      {
+      key: "4",
+      name: "All git repositories",
+      codeLang: "Java, C#, Python, Shell, C++",
+      description:
+        "This is a direct link to my account at Github.com. It should be possible to see other projects and some school assignments I have worked on.",
+      link: "https://github.com/Bass4Nation",
+    },
   ];
+
+  
 
   return (
     <>
