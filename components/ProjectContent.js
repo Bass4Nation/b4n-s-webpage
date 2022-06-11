0d0d2fb2f6002a5eec1450806747a9c13e752f49
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-06-11 13:25:49 +0200

    Trying to add flip cards on projects

--- components/ProjectContent.js
+++ components/ProjectContent.js
@@ -1,16 +1,29 @@
 import Link from "next/link";
+import Image from 'next/image'
 import styles from "../styles/Main.module.css";
 
-const ProjectContent = ({ title, codeLang, description, projectLink }) => {
+
+const ProjectContent = ({ title, codeLang, description, projectLink, fullName }) => {
+
+    let previewImg = "https://raw.githubusercontent.com/" + fullName + "/main/pictures/preview.png"
+
     return (
         <>
             <section className={styles.projectCard}>
-                <h3>Title: {title}</h3>
-                <p>Language used: {codeLang}</p>
-                <p>Description: {description}</p>
-                <p>Github link to this project: <Link href={projectLink} passHref><a className={styles.projectLinks}>click here</a></Link></p>
+                <section className={styles.flip}>
+                    <section className={styles.front}>
+                        <h3>Title: {title}</h3>
+                        <p>Language used: {codeLang} and more</p>
+                        <p>Description: {description}</p>
+                        <p>Github link to this project click on picture on hover</p>
+                    </section>
+                    <section className={styles.back}>
+                    <Link href={projectLink} passHref>
+                        <img src={previewImg} />
+                        </Link>
+                    </section>
+                </section>
             </section>
-
         </>
     )
 
--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,7 @@
 import ProjectContent from "../components/ProjectContent";
 import style from "../styles/Main.module.css"
+
+
 // This gets called on every request
 export async function getServerSideProps() {
   // Fetch data from external API
@@ -9,66 +11,15 @@ export async function getServerSideProps() {
   return { props: { data } }
 }
 
+
+
 const Projects = ({ data }) => {
-  const projectData = [
-    {
-      key: "1",
-      name: "B4N.no Webpage",
-      codeLang: "Next.js, HTML, CSS, JavaScript",
-      description:
-        "This webpage. Wanted to make this webpage because it could show some of my coding skills.",
-      link: "https://github.com/Bass4Nation/b4n-s-webpage",
-      picture: "/public/drone_controller.png",
-    },
-    {
-      key: "2",
-      name: "Tello SDK Drone Controller ",
-      codeLang: "C#",
-      description:
-        "Got a small programmable drone at home. It uses a UDP client to send information on what it should do." +
-        " etl",
-      link: "https://github.com/Bass4Nation/Tello-Drone",
-      picture: "/public/drone_controller.png",
-    },
-    {
-      key: "3",
-      name: "F1Tenth Autonomous Driving Car (Complete Bachlor Assignment)",
-      codeLang: "Python, Shell, C++",
-      description:
-        "Self driven RC car, 2D lidar mapping, also possible with manuell control",
-      link: "doesn't have a github link",
-      picture: "/public/drone_controller.png",
-    },
-    {
-      key: "4",
-      name: "All git repositories",
-      codeLang: "Java, C#, Python, Shell, C++",
-      description:
-        "This is a direct link to my account at Github.com. It should be possible to see other projects and some school assignments I have worked on.",
-      link: "https://github.com/Bass4Nation",
-      picture: "/public/drone_controller.png",
-    },
-  ];
   console.log(data)
 
   return (
     <>
       <section>
         <h3> Projects page</h3>
-        <section className={style.projectCards}>
-          {projectData.map((data) => (
-            <ProjectContent
-              key={data.key}
-              title={data.name}
-              codeLang={data.codeLang}
-              description={data.description}
-              projectLink={data.link}
-            />
-          ))}
-
-        </section>
-        <h3>This is the rest of my github</h3>
-
         <section className={style.projectCards}>
           {data.map((git) => (
             <ProjectContent
@@ -77,10 +28,10 @@ const Projects = ({ data }) => {
               codeLang={git.language}
               description={git.description}
               projectLink={git.svn_url}
+              fullName={git.full_name}
             />
           ))}
         </section>
-
       </section>
     </>
   );
