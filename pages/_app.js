38225571b2475cd6913504d0f551247fa7cdf8c5
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-06-16 13:08:07 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,16 @@
-import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
 
-  const [ constState, setConstState] = useState(false)
-
-  // Wants to add possibillity to change language on my page. From english to Norwegian.
-  const [ langState, setLangState] = useState("en")
-
-  // Message that will popup if constState is true
-  if(constState == true){
-    alert("This page is under construction.")
-  }
-
-
   return (
     <>
+      <style jsx global>{
+        `body {
+          margin: 0px;
+          padding: 0px;
+         }`}
+      </style>
       <Layout>
         <Component {...pageProps} />
       </Layout>
