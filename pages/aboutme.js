96e58d8a483ddb27514906b3c2505a8c52eb9e12
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-06-17 15:35:48 +0200

    Update aboutme.js
    Fixed a ' error

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -3,7 +3,7 @@ const AboutMe = () => {
     <>
       <h3>About me page</h3>
       <p>Hi. I&apos;m Kristoffer Snopestad Søderkvist, and are now seeking a job in the IT universe. I Have finished my bachelor assignment, and are currently (17.june) 
-        waiting for answere on other exam's I had. I like to code most
+        waiting for answere on other exam&apos;s I had. I like to code most
         language I&apos;ve tried yet, but some better than other like JavaScript with
         framework such as Node.js, React.js, NEXT.js. Also like C# and Java. </p>
       <p>But when I get some freetime I like to play videogames on my computer, and watch and try to learn some japanese language. Plus sometime I like to start on my own projects 
