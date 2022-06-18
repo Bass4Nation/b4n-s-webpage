b1681bc23f0465c097c7cf36fff3c6196d0225d1
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-06-18 13:23:57 +0200

    A bit styling for patchnotes

--- components/Updates.js
+++ components/Updates.js
@@ -1,9 +1,10 @@
 import { useGit } from "../hooks/useGit";
+import stylesDark from "../styles/DarkMain.module.scss";
 
 
 const Updates = () => {
     const { allPatch } = useGit()
-    
+
     const timeFormat = (stringTime) => {
         let strTime = stringTime.replace("Z","")
         const [dateRelated, timeRelated] = strTime.split('T');
@@ -18,16 +19,18 @@ const Updates = () => {
         return created_date;
     }
 
+    const styles = stylesDark;
+
     return (
         <>
         <section>
             <h3>Patch notes sent to Github. Push messages</h3>
-            <section>
+            <section className={styles.patchnotes}>
             {allPatch.map((arg) => (
-                    arg.payload.commits.map((arg1) => (<>
+                    arg.payload.commits.map((arg1) => (<section className={styles.patchnote}>
                     <p>{arg1.message}</p>
                     <p>{timeFormat(arg.created_at)}</p>
-                    </>
+                    </section>
                     ))
                 ))}
             </section>
